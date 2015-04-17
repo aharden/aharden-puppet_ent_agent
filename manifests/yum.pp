65a9f2f83da882397b59bd321d33b965e901186b
Alex Harden <aharden@te.com>
2015-04-17 09:37:49 -0400

    removed attempt at yumrepo fix

--- manifests/yum.pp
+++ manifests/yum.pp
@@ -1,22 +1,9 @@
 class puppet_ent_agent::yum inherits puppet_ent_agent {
-  File_line['patch bug PUP-2271'] -> Yumrepo['puppetlabs-pepackages']
 
   if $package_ensure == 'latest' {
     $package_version = 'current'
   } else {
     $package_version = $package_ensure
-  }
-
-  #patch bug PUP-2271 if it's there on PE 3.3 by updating yumrepo.rb
-  if ($::pe_major_version == '3' and $::pe_minor_version == '3') {
-    file { 'patch bug PUP-2271':
-      ensure => 'file',
-      path   => '/opt/puppet/lib/ruby/site_ruby/1.9.1/puppet/type/yumrepo.rb',
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-      source => "puppet:///modules/${module_name}/puppet-enterprise.gpg.key",
-    }
   }
 
   yumrepo { 'puppetlabs-pepackages':
@@ -25,7 +12,7 @@ class puppet_ent_agent::yum inherits puppet_ent_agent {
     enabled   => true,
     gpgcheck  => true,
     gpgkey    => "https://${master}:8140/packages/GPG-KEY-puppetlabs",
-    proxy     => '_none_',
+    #proxy     => '_none_',
     sslverify => 'False',
     before    => Package['pe-agent'],
   }
