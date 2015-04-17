d9f76bb91d85e9121a0861679c2cda2a41f3918f
Alex Harden <aharden@te.com>
2015-04-17 09:07:44 -0400

    patching yumrepo.rb to allow proxy=_none_

--- manifests/yum.pp
+++ manifests/yum.pp
@@ -1,8 +1,19 @@
 class puppet_ent_agent::yum inherits puppet_ent_agent {
+  File_line['patch bug PUP-2271'] -> Yumrepo['puppetlabs-pepackages']
+
   if $package_ensure == 'latest' {
     $package_version = 'current'
   } else {
     $package_version = $package_ensure
+  }
+
+  #patch bug PUP-2271 if it's there on PE 3.3
+  if ($::pe_major_version == '3' and $::pe_minor_version == '3') {
+    file_line { 'patch bug PUP-2271':
+      path  => '/opt/puppet/lib/ruby/site_ruby/1.9.1/puppet/type/yumrepo.rb',
+      match => "      next if value.to_s == 'absent'",
+      line  => '      next if value.to_s =~ /^(absent|_none_)$/',
+    }
   }
 
   yumrepo { 'puppetlabs-pepackages':
