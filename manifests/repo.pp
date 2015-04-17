2a9627673e16726cdccc4f162399d90ffc9d3607
Alex Harden <aharden@te.com>
2015-04-17 10:38:58 -0400

    adjusted yumrepo for pe 3.3

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -8,12 +8,12 @@ class puppet_ent_agent::repo inherits puppet_ent_agent {
   case $::osfamily {
     'RedHat': {
       # yumrepo on PE 3.3 doesn't support proxy => _none_ - PUP-2271
-      if ($::pe_major_version == '3' and $pe_minor_version == '3') {
+      if ($::pe_major_version == '3' and $::pe_minor_version == '3') {
         file { '/etc/yum.repos.d/pe_repo.repo':
-          owner    => 'root',
-          group    => 'root',
-          mode     => '0644',
-          template => "puppet:///modules/${module_name}/pe_repo.repo.erb",
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => template("${module_name}/pe_repo.repo.erb"),
         }
       } else {
         contain puppet_ent_agent::yum
