24ed78678d5803ddf35f015ba70ca53a3910dd14
Alex Harden <aharden@te.com>
2015-04-29 06:33:37 -0400

    adjusted installer for WS2003 x64

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -2,6 +2,8 @@
 # The Windows pe-agent files are not hosted in pe_repo, so we use a UNC share
 #  to hold the PE agent MSIs, which are installed by the Windows package
 #  provider
+# Note: Must use 32-bit installer on Windows Server 2003 64-bit:
+#  https://docs.puppetlabs.com/pe/latest/install_windows.html#installdir
 class puppet_ent_agent::windows inherits puppet_ent_agent {
   $master         = $::puppet_ent_agent::master
   $version        = $::puppet_ent_agent::version
@@ -13,8 +15,16 @@ class puppet_ent_agent::windows inherits puppet_ent_agent {
       $package_name = 'Puppet Enterprise'
     }
     'x64': {
-      $package_msi  = "puppet-enterprise-${version}-x64.msi"
-      $package_name = 'Puppet Enterprise (64-bit)'
+      case $::kernelversion {
+        '5.2.3790': { # must install 32-bit on WS2003 x64
+          $package_msi  = "puppet-enterprise-${version}.msi"
+          $package_name = 'Puppet Enterprise'
+        }
+        default: {
+          $package_msi  = "puppet-enterprise-${version}-x64.msi"
+          $package_name = 'Puppet Enterprise (64-bit)'
+        }
+      }
     }
     default: {
       notify { "Unsupported Windows architecture ${::architecture}.": }
