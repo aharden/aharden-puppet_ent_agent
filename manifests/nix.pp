7899d424c3944461b998e9714c3de048ced32f7a
Alex Harden <aharden@te.com>
2015-07-10 14:46:02 -0400

    PUP-293 refactored debian

---
+++ manifests/nix.pp
@@ -0,0 +1,48 @@
+# Unix/Linux agent installation
+# We use a modification of the PE curl-based installation
+class puppet_ent_agent::nix inherits puppet_ent_agent {
+  $staging_dir = $::puppet_ent_agent::staging_dir
+  $version     = $::puppet_ent_agent::ensure
+
+  case $::osfamily {
+    'AIX':     { $os = 'aix' }
+    'Debian':  {
+      case $::platform_tag {
+        'ubuntu-14.04-i386','ubuntu-14.04-amd64' : { $os = 'ubuntu-14' }
+        default : {
+          notify { "Unsupported Debian platform ${::platform_tag}.": }
+        }
+      }
+    }
+    'Solaris': {
+      case $::platform_tag {
+        'solaris-10-i386','solaris-10-sparc' : { $os = 'solaris-10'}
+        'solaris-11-i386','solaris-11-sparc' : { $os = 'solaris-11'}
+        default : {
+          notify { "Unsupported Solaris platform ${::platform_tag}.": }
+        }
+      }
+    }
+  }
+
+  if $::pe_version != $version {
+
+    file { $staging_dir:
+      ensure => directory,
+      owner  => 'root',
+      group  => 'system',
+      mode   => '0755',
+    }
+
+    file { "${staging_dir}/${os}-install.bash":
+      ensure  => file,
+      owner   => 'root',
+      group   => 'system',
+      mode    => '0644',
+      content => template("${module_name}/${os}-install.bash.erb"),
+    } ->
+    exec { "/bin/bash -e ${staging_dir}/${os}-install.bash":
+      user => 'root',
+    }
+  }
+}
