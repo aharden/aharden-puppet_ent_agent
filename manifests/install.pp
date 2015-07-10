7722c919817787dcd699f71c0928ad4981ceff45
Alex Harden <aharden@te.com>
2015-07-10 12:23:06 -0400

    PUP-293 moved conditional to where really needed

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,36 +5,34 @@ class puppet_ent_agent::install inherits puppet_ent_agent {
 
   include ::puppet_ent_agent::repo
 
-  if $ensure != 'present' {
-    case $::osfamily {
-      'AIX','Solaris','Windows': {
-        if $ensure != 'latest' {
-          case $::osfamily {
-            'AIX':     { include ::puppet_ent_agent::aix }
-            'Solaris': { include ::puppet_ent_agent::solaris }
-            'windows': {
-              if $windows_source {
-                include ::puppet_ent_agent::windows
-              }
-              else {
-                notify { 'Windows repository not available: source not defined.': }
-              }
+  case $::osfamily {
+    'AIX','Solaris','Windows': {
+      if ($ensure != 'present') and ($ensure != 'latest') {
+        case $::osfamily {
+          'AIX':     { include ::puppet_ent_agent::aix }
+          'Solaris': { include ::puppet_ent_agent::solaris }
+          'windows': {
+            if $windows_source {
+              include ::puppet_ent_agent::windows
             }
-            default: {}
+            else {
+              notify { 'Windows repository not available: source not defined.': }
+            }
           }
-        }
-        else {
-          notify { "Must specify PE agent version on ${::osfamily}": }
+          default: {}
         }
       }
-      'Debian','RedHat' : {
-        package { 'pe-agent':
-          ensure  => $ensure,
-        }
+      else {
+        notify { "Must specify PE agent version on ${::osfamily}": }
       }
-      default: {
-        notify { "Unsupported OS family ${::osfamily}.": }
+    }
+    'Debian','RedHat' : {
+      package { 'pe-agent':
+        ensure  => $ensure,
       }
+    }
+    default: {
+      notify { "Unsupported OS family ${::osfamily}.": }
     }
   }
 }
